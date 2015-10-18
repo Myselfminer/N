61b9272dd446c7a11176e61a0f82f133677403ad
Myselfminer <david@divaproject.com>
2015-10-18 12:13:50 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+ip="localhost"
+port=25529
+online_mode=True
+maxplayers=10
+max_jump=100
+kick_for_illegal_pos=True
+backlog=0
+install_path="D:\\Privat\\N\\"#please replace \ with \\
+#Do not edit the file below this line!
+def get(var):
+    if var=="ip":
+        return ip
+    if var=="port":
+        return port
+    if var=="online-mode":
+        return online-mode
+    if var=="maxplayers":
+        return maxplayers
+    if var=="max_jump":
+        return max_jump
+    if var=="kick_for_illegal_pos":
+        return kick_for_illegal_pos
+    if var=="backlog":
+        return backlog
+    if var=="install_path":
+        return install_path
